9556f825271c551c4b0237cce4e08a5dcd13e799
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-05-24 16:55:55 +0600

    Database queries with transactions & aggregators

--- schema.sql
+++ schema.sql
@@ -8,5 +8,6 @@ CREATE TABLE animals (
     date_of_birth DATE,
     escapte_attempts INT,
     neutered BOOLEAN,
-    weight_kg DECIMAL(10,2)
+    weight_kg DECIMAL(10,2),
+    species VARCHAR(100)
 );
